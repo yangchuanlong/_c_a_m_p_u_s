ffbf1866c43a445cedef75a2a31fba0e8842a265
Administrator <yang_chuanlong@163.com>
2019-02-11 01:36:21 +0800

    获取用户信息

--- pages/questionEdit/questionEdit.js
+++ pages/questionEdit/questionEdit.js
@@ -1,13 +1,16 @@
-const { globalData } = getApp();
+const app = getApp(), globalData = app.globalData;
 Page({
   data: {
+    userInfo: {},
     title: "",
     types: [],
     content: "",
     loading: false,
-    disabled: false
+    disabled: false,
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
-  onAddTxt(){clolou
+  onAddTxt(){
     globalData.inputedTxt = null;
     wx.navigateTo({
       url: '/pages/editor/editor',
@@ -66,5 +69,39 @@ Page({
       title: query.title,
       types: query.types.split(",")
     });
-  }
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
 });
