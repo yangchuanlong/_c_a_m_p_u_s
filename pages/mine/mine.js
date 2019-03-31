b992f883eef9c8658ebb221d9f37ce19d1cdd594
Administrator <yang_chuanlong@163.com>
2019-03-31 10:42:16 +0800

    我的页面tab滑动

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -2,12 +2,15 @@ import config from '../../utils/config.js';
 Page({
     data: {
         myQuestions: [],
-        tabIndex: 0
+        activeTab: 0,
+        touchStartX: 0,
+        touchStartY: 0,
+        tabs: ["我的问题", "我的回答", "我的收藏"]
     },
     onTabClick(evt) {
-      const tabIndex = evt.target.dataset.tabIndex;
+      const activeTab = evt.target.dataset.activeTab;
       this.setData({
-        tabIndex
+          activeTab
       });
     },
     onLoad() {
@@ -36,5 +39,30 @@ Page({
           })
         }
       })
+    },
+    onTouchStart(evt) {
+        const touch = evt.touches[0];
+        this.setData({
+            touchStartX: touch.clientX,
+            touchStartY: touch.clientY
+        });
+    },
+    onTouchEnd (evt){
+        const {touchStartX, touchStartY, activeTab, tabs} = this.data;
+        const touch = evt.changedTouches[0];
+        const deltaX = Math.abs(touch.clientX - touchStartX);
+        const deltaY = Math.abs(touch.clientY - touchStartY);
+        if(deltaX > 2 * deltaY) {
+            if(touch.clientX > touchStartX) {
+                this.setData({
+                    activeTab: activeTab == 0 ? tabs.length - 1 : activeTab - 1
+                })
+            } else {
+                console.log("左滑")
+                this.setData({
+                    activeTab: (activeTab + 1) % tabs.length
+                })
+            }
+        }
     }
 });
