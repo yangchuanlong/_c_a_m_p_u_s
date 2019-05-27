d65b11371f81a0e09fb1930511a1307d34d0b52f
Administrator <yang_chuanlong@163.com>
2019-05-27 23:38:22 +0800

    prod config

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,12 @@
 
 const appConfig = {
-  env: 'campus',
+  env: 'prod-xychb',
   version: '0.1.0',
   administrators: {
     super: [//超级管理员
-      'oH3zy5Gdi9JyWq-1SbgzBMDsCAsE',
-      'oH3zy5N9Z7N-zNmtW6XiYAmbbIYI',
-      'oH3zy5F2K3rG1MuscMSkgHy-g2vU'
+      'orw4d5H_XMCnTBSJhm6nfLsDxAgE', //feidao
+      'orw4d5H_XMCnTBSJhm6nfLsDxAgE', //zulong
+      'orw4d5ObTutNLtRvCj4_9nH49YzM'
     ],
     collegesAdmin:{//collegeId: []
       1: [
